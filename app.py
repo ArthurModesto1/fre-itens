549f5dd64a39569ca385c2fa5d80970f2cbd3098
Arthur Modesto <86253233+ArthurModesto1@users.noreply.github.com>
2026-03-10 10:49:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -117,14 +117,11 @@ if selected_item in DOWNLOAD_FILES:
                )
 
            st.markdown("### 📊 Prévia dos dados")
+
            preview_df = df_filtered_dl.head(5)
            st.dataframe(
-                preview_df, 
-                use_container_width=True, 
-                column_config={
-                    col: st.column_config.Column(alignment="center")
-                    for col in preview_df.columns
-                }
+                preview_df.style.set_properties(**{'text-align': 'center'}), 
+                use_container_width=True
             )
 
        else:
